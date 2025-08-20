bf8d0d5529d33df414d02cb1028c8d7cd0766af6
zubair-phiwallet <108828255+zubair-phiwallet@users.noreply.github.com>
2025-08-20 15:05:32 +0400

    init

---
+++ lib/org/gluu/agama/pw/PasswordService.java
@@ -0,0 +1,17 @@
+package org.gluu.agama.pw;
+
+import org.gluu.agama.pw.jans.JansPasswordService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class PasswordService {
+
+    public abstract boolean validate(String username, String password);
+
+    public abstract String lockAccount(String username);
+
+    public static PasswordService getInstance(HashMap config) {
+        return new JansPasswordService(config);
+    }
+}
